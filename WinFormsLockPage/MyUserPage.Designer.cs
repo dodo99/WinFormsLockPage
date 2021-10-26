5f99b547b622d9db4e4b71a7da3be7fabfb6d8b7
Tony Tang <tony.tang@semilab.com>
2021-10-26 10:17:23 -0400

    re-position the UI controls location

--- WinFormsLockPage/MyUserPage.Designer.cs
+++ WinFormsLockPage/MyUserPage.Designer.cs
@@ -39,6 +39,11 @@ namespace WinFormsLockPage
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "List Item1",
+            "List Item2",
+            "List Itme3",
+            "List Item4"});
             this.listBox1.Location = new System.Drawing.Point(42, 146);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(120, 95);
@@ -47,7 +52,7 @@ namespace WinFormsLockPage
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(42, 98);
+            this.checkBox1.Location = new System.Drawing.Point(42, 111);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(80, 17);
             this.checkBox1.TabIndex = 1;
@@ -68,7 +73,7 @@ namespace WinFormsLockPage
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(328, 109);
+            this.label1.Location = new System.Drawing.Point(42, 85);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 3;
@@ -76,7 +81,7 @@ namespace WinFormsLockPage
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(369, 109);
+            this.textBox1.Location = new System.Drawing.Point(83, 85);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 4;
